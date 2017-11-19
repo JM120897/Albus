1c54f87f47adee49b62715244919c530bbf74025
usuario <fernandezfernando97@gmail.com>
2017-11-19 17:16:57 +0100

    Modificado el Servicio

--- servicio.py
+++ servicio.py
@@ -1,5 +1,36 @@
-'''
-Created on 19 nov. 2017
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (http://tiny.be). All Rights Reserved
+#    
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
 
-@author: jayem
-'''
+from osv import osv
+from osv import fields
+
+class servicio(osv.Model):
+
+    _name = 'servicio'
+    _description = 'Informacion sobre el servicio'
+ 
+    _columns = {
+            
+            'descripcion':fields.char('Descripcion', size=150, required=False, readonly=False),
+          
+        }
+servicio()
