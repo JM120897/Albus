56c1a1b17f0d8e1c3be8497af837a8b27906f74c
JM120897 <JM120897@gmail.com>
2017-11-23 16:49:30 +0100

    Vacaciones ahora tiene su campo relacional

--- vacaciones.py
+++ vacaciones.py
@@ -32,6 +32,7 @@ class vacaciones(osv.Model):
             'name':fields.char('Descripcion', size=150, required=False, readonly=False),
             'fechaIni':fields.date('Fecha Inicio', required=False, readonly=False),
             'fechaFin':fields.date('Fecha Fin', required=False, readonly=False),
+            'conductor_id':fields.many2many('conductor','conductor_vacaciones_rel','vacacion_id','conductor_id','Conductores')
            
           
             
