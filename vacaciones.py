21770cc269b5b086f819e8fb8ae02bef9782d478
JM120897 <JM120897@gmail.com>
2017-11-30 14:12:06 +0100

    Added search: alquiler, cliente, conductor cuenta, vacaciones y servicio

--- vacaciones.py
+++ vacaciones.py
@@ -29,7 +29,8 @@ class vacaciones(osv.Model):
     _description = 'Informacion sobre los vacaciones'
  
     _columns = {
-            'name':fields.char('Descripcion', size=150, required=False, readonly=False),
+            'name':fields.char('ID', size=150, required=False, readonly=False),
+            'descripcion':fields.char('Descripcion', size=150, required=False, readonly=False),
             'fechaIni':fields.date('Fecha Inicio', required=False, readonly=False),
             'fechaFin':fields.date('Fecha Fin', required=False, readonly=False),
             'conductor_id':fields.many2many('conductor','conductor_vacaciones_rel','vacacion_id','conductor_id','Conductores')
