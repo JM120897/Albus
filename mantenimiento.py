3036831406daa395e2a41d2e8e63d529fc014f27
unknown <fernandezfernando97@gmail.com>
2017-12-22 16:42:47 +0100

    Añadida restriccion de precio a mantenimiento

--- mantenimiento.py
+++ mantenimiento.py
@@ -3,7 +3,7 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2010 Tiny SPRL (http://tiny.be). All Rights Reserved
-#    
+#
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -23,13 +23,24 @@ from osv import osv
 from osv import fields
 
 class mantenimiento(osv.Model):
+
+
+    def _check_precio(self, cr, uid, ids):
+        # Los servicios tienen que tener almenos 0.1 KM para poder registrarse
+        serv=self.browse(cr, uid, ids[0],context=None)
+        if serv.precio <= 0:
+            return False
+        return True
+
     _name = 'mantenimiento'
     _description = 'Clase para el mantenimiento de un autobus'
- 
+
     _columns = {
             'name':fields.text('Descripcion'),
             'precio':fields.float('Precio', required=True),
             'fecha':fields.date('Fecha', required=True),
             'matricula_id':fields.many2one("autobus","Matricula"),
         }
+
+    _constraints = [(_check_precio, 'El precio del mantenimiento no puede ser menor de 0' , [ 'precio'])]
 mantenimiento()
