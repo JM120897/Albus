1380b550ca9c728a34fe70dbe26ab26091c99b1d
JM120897@gmail.com <jayem@192.168.1.134>
2017-12-28 17:37:23 +0100

    Añadidos constraints

--- cliente.py
+++ cliente.py
@@ -23,6 +23,15 @@ from osv import osv
 from osv import fields
 
 class cliente(osv.Model):
+    
+    #Intento de check de cuenta que no llego a funcionar
+    def _check_cuenta(self, cr, uid, ids, context=None):
+        cliente = self.browse(cr, uid, ids[0], context=None) 
+        for cuenta in cliente.cuenta_id:
+            if(cuenta.conductor_id is not None):
+                return False
+            else:
+                return True
 
     _name = 'cliente'
     _description = 'Informacion sobre Cliente'
@@ -36,7 +45,7 @@ class cliente(osv.Model):
         'cuenta_id':fields.many2many('cuenta','cuenta_cliente_rel','cliente_id','cuenta_id','Cuentas'),
         'alquiler_id':fields.one2many("alquiler","cliente_id","Alquileres")
         }
-    
+    #_constraints = [(_check_cuenta, 'Alguna de las cuentas pertenece a un conductor' , [ 'cuenta_id'])]
     _sql_constraints = [('dni_uniq_cliente', 'unique (name)', 'Ya existe un usuario con ese DNI'),]
     
 cliente()
